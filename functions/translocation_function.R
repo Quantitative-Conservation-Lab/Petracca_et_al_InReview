8b8405d66a98b158ef4cf34974e9df693a89f970
Lisanne <lisanne.petracca@yahoo.com>
2025-03-12 12:51:41 -0500

    minor comment edits

--- functions/translocation_function.R
+++ functions/translocation_function.R
@@ -1,15 +1,15 @@
 get.translocations <- function(n.wolves.transl.fxn, EWash, site) {
+  #in this function, we are removing wolves by pack in E Washington and translocating them to one of two sites -- Mt Ht Helens or the Olympic Peninsula 
   
   dim(n.wolves.transl.fxn)
   
   if(site=="St Helens"){
-    packs_translocate <- c(14,10) #these are the packs that qualify based on the RSF [I personally did not trust occupancy]
+    packs_translocate <- c(14,10) #these are the packs that qualify based on the RSF & correspond to territory IDs
   }
   if(site=="Olympic"){
     packs_translocate <- c(145,125)
   }
   
-  #in this function, we are removing wolves by pack in E Washington based on a removal rate
   Nsamples <- dim(n.wolves.transl.fxn)[1]
   
   pup.translocated.all <- ad.translocated.all <- matrix(0, nrow=Nsamples, ncol=224)
@@ -19,7 +19,7 @@ get.translocations <- function(n.wolves.transl.fxn, EWash, site) {
     pup.translocated <- rep(0,224)
     ad.translocated <- rep(0,224)
     
-      #THIS IS WHERE TRANSLOCATION HAPPENS, AT YEAR 3 {ARBITRARILY SET}
+      #THIS IS WHERE TRANSLOCATION HAPPENS
           #goal is to take 2 adults in age class 3 and 2 adults in pup class and put them in territories 14 and 23
           #let's make sure we'd leave at least two adults and one pup behind after the translocation
           packs_qualify_intermed <- which(n.wolves.transl.fxn[i,3,EWash]>=4 & n.wolves.transl.fxn[i,1,EWash]>=3)
@@ -33,7 +33,7 @@ get.translocations <- function(n.wolves.transl.fxn, EWash, site) {
               pup.translocated[packs_translocate] <- 2
               ad.translocated[packs_translocate] <- 2
             
-            pup.translocated.all[i,] <- pup.translocated #these sad immigs are those that have been rejected based on bernoulli draw
+            pup.translocated.all[i,] <- pup.translocated 
             ad.translocated.all[i,] <- ad.translocated 
 
               }#close else
